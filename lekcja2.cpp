313837b9e365a52b41d9787b75fdb98807ee04dc
sally027 <kornelianekandatrepka@gmail.com>
2023-10-30 12:05:22 +0100

    komentarz

---
+++ lekcja2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	short a,b;
+	cout<<"podaj a:" ;
+	cin>> a;
+	cout<<"podaj b: ";
+	cin >>b;
+	if(a>b){ // instrukcja warunkowa jeżeli ...
+	
+	 cout<<"a>b";
+}
+else{ // w przeciwnnym razie
+	cout <<"a<b";
+}
+	 
+}
